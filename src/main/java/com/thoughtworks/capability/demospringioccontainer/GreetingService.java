e1684f1b7138411f3cdec0763870343bedc655d8
hejie <565703319@qq.com>
2020-11-07 22:43:04 +0800

    [hj] add lookup annotation and service log

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,18 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(SCOPE_PROTOTYPE)
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("------GreetingService-------");
+    }
 
     String sayHi() {
         return "hello world";
